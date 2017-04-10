f297fb8cda3421cc1ae5d32cdeee4896f189c8a0
akshikabaluapuri <akshikabaluapuri@gmail.com>
2017-04-10 16:16:59 -0400

    modified first commit

--- plot2.R
+++ plot2.R
@@ -11,18 +11,13 @@ electric_data <- fread(electric_file, na.strings = "?", stringsAsFactors = FALSE
                        skip = 66637, nrows = 2880)
 
 
-#electric_data$V1<- as.Date(electric_data$V1, format = "%d/%m/%y")
-
+#extracted data
 ele_data2 <- electric_data %>% mutate(V1 = as.POSIXct(dmy_hms(as.character(paste(V1 ,V2)))),
-                        V3 = as.numeric(as.character(V3))) %>% select(V1,V3)
+                                      V3 = as.numeric(as.character(V3))) %>% select(V1,V3)
 
-#plot(strptime(electric_data$timestamp,"%d/%m/%y %H:%M:%S"),
- #    electric_data$V3, type = 1,xlab = "", ylab = "Global Active Power(Kilowats)")
+#ploting
 with(ele_data2, plot(V1,V3, type = "l", xlab = " ", ylab = "Global Active Power(Kilowats)"))
 
-
+# png file
 dev.copy(png, file = "electric/plot2.png", width =480, height = 480)
 dev.off()
-#png("plot2.png", width = 480, height = 480)
-#plot(ele_data2,type = "l",xlab = " ", ylab = "Global Active Power(Kilowats)")
-#dev.off()
